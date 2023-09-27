1fdccfda9cc3358dca4da7b402fd646e657bf6a6
FakirHerif <basdemir35@hotmail.com.tr>
2023-09-27 23:26:30 +0300

    4-javascript temelleri 10.konu temeller dördüncü bölümde yer alan alıştırmaların birincisi olan 01_helloWorld çözümü eklendi

---
+++ 4-JAVASCRIPT TEMELLERİ/10- TEMELLER DÖRDÜNCÜ KISIM ALIŞTIRMA ÇÖZÜMLERİ/01_helloWorld.js
@@ -0,0 +1,14 @@
+// BU KISIMDA 10- TEMELLER DÖRDÜNCÜ KISIM'DA YER ALAN GÖREV BAŞLIĞI ALTINDAKİ ALIŞTIRMALARIN ÇÖZÜMLERİ YER ALMAKTADIR.
+
+// TEST AŞAMALARI BULUNMAMAKTADIR. TESTLERE https://github.com/TheOdinProject/javascript-exercises/tree/main SAYFASINDAN ULAŞABİLİRSİNİZ.
+
+// --- SADECE ALIŞTIRMADA NE YAPILMASI İSTENDİĞİ VE ÇÖZÜM YER ALMAKTADIR. ---
+
+// 'Hello, World!' ÇIKTISI ALDIĞINIZ BİR FONKSİYON OLUŞTURUN:
+
+const helloWorld = function() {
+    return 'Hello, World!'
+  };
+
+
+
